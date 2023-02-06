e24bcaaf523e8d0ad184a1fdc1f9c7922ace84fd
Serafim Bogomolov <srf.bogomolov@protonmail.com>
2023-02-06 21:45:47 +0300

    Separate views from logic using templates
    Move the HTML code to a separate `chat.html` file in a `templates` directory.
    
    Implement the `templateHandler` struct using `text/template` and `sync` packages
    that is responsible for loading, compiling, and delivering template. The
    `templateHandler` type has a single method called `ServeHTTP` that will load the
    source file, compile the template and execute it, and write the output to the
    specified `http.ResponseWriter` method.

--- main.go
+++ main.go
@@ -3,21 +3,29 @@ package main
 import (
     "log"
     "net/http"
+    "path/filepath"
+    "sync"
+    "text/template"
 )
 
-func main() {
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        w.Write([]byte(`
-            <html>
-                <head>
-                    <title>Chat</title>
-                </head>
-                <body>
-                    Let's chat!
-                </body>
-            </html>
-        `))
+// templateHandler represents a single template
+type templateHandler struct {
+    once     sync.Once
+    filename string
+    templ    *template.Template
+}
+
+// ServeHTTP handles the HTTP request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    t.once.Do(func() {
+        t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
     })
+    t.templ.Execute(w, nil)
+}
+
+func main() {
+    // root
+    http.Handle("/", &templateHandler{filename: "chat.html"})
 
     // start the web server
     if err := http.ListenAndServe(":8080", nil); err != nil {
